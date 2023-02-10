fedada26ccec90a5cb5c60e952d10bf1658123f4
Dima <dm.katasonov1@gmail.com>
2023-02-10 18:35:14 +0300

    Работа дома

--- HomeWork_2.cs
+++ HomeWork_2.cs
@@ -89,3 +89,158 @@
 //     }
 //     return myArray;
 // }
+
+// Домашние задания к семинару 5 
+
+
+// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. 
+// Напишите программу, которая покажет количество чётных чисел в массиве.
+
+// [345, 897, 568, 234] -> 2
+
+
+// int[] array = GetArray(20, 100, 999);
+// Console.WriteLine($"[{String.Join ("," , array)}]");
+// Console.WriteLine($"Количество чётных чисел в массиве {countEl(array)}");
+
+// int countEl(int[] array)
+// {
+//     int count = 0;
+//     foreach (int el in array)
+//     {
+//         if (el % 2 == 0)
+//         {
+//             count++;
+//         }
+//     }
+//     return count;
+// }
+
+
+// // ----------Метод------------
+
+// int[] GetArray(int size, int minValue, int maxValue)
+// {
+//     int[] res = new int[size];
+
+//     for (int i = 0; i < size; i++)
+//     {
+//         res[i] = new Random().Next(minValue, maxValue + 1);
+//     }
+//     return res;
+// }
+
+
+// Задача 36: Задайте одномерный массив, заполненный случайными числами. 
+// Найдите сумму элементов, стоящих на нечётных индексах.
+
+// [3, 7, 23, 12] -> 19
+
+// [-4, -6, 89, 6] -> 0
+
+
+// int[] array = GetArray(10, -100, 100);
+// Console.WriteLine($"[{String.Join ("," , array)}]");
+// Console.WriteLine($"Cумма элементов, стоящих на нечётных индексах: {sumIndex(array)} ");
+
+// int sumIndex(int[] array)
+// {
+//     int sum = 0;
+//     for (int i = 0; i < array.Length; i++)
+//     {
+//         if (i % 2 != 0)
+//         {
+//             sum = sum + array[i];
+//         }
+//     }
+//     return sum;
+// }
+
+// // ----------Метод------------
+
+// int[] GetArray(int size, int minValue, int maxValue)
+// {
+//     int[] res = new int[size];
+
+//     for (int i = 0; i < size; i++)
+//     {
+//         res[i] = new Random().Next(minValue, maxValue + 1);
+//     }
+//     return res;
+// }
+
+// Задача 37: Найдите произведение пар чисел в одномерном массиве. 
+// Парой считаем первый и последний элемент, второй и предпоследний и т.д. 
+// Результат запишите в новом массиве.
+
+// [1 2 3 4 5] -> 5 8 3
+
+// [6 7 3 6] -> 36 21
+
+
+
+// int[] array = GetArray(5, -10, 10);
+// Console.WriteLine($"[{String.Join ("," , array)}]");
+// Console.WriteLine($"{String.Join ("," , SumIndex(array))}");
+
+
+// int[] SumIndex(int[] array)
+// {   
+//     int size = array.Length / 2; 
+//     if (array.Length % 2 == 1) size++; 
+//     int[] result = new int [size];
+//     for (int i = 0; i < array.Length / 2; i++)
+//     {
+//         result[i] = array [i] * array[array.Length - 1 - i]; 
+//     }
+//     if (array.Length % 2 == 1) result[size - 1] = array[array.Length / 2];
+//     return result;
+// }
+
+// // ----------Метод------------
+
+// int[] GetArray(int size, int minValue, int maxValue)
+// {
+//     int[] res = new int[size];
+
+//     for (int i = 0; i < size; i++)
+//     {
+//         res[i] = new Random().Next(minValue, maxValue + 1);
+//     }
+//     return res;
+// }
+
+// ДЗ по 6 семинару 
+
+// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел строго больше 0 ввёл пользователь.
+
+// 0, 7, 8, -2, -2 -> 2
+
+// 1, -7, 567, 89, 223-> 4
+
+// Для строки можно использовать данный метод string[] numS = stringArray.Split(' ', StringSplitOptions.RemoveEmptyEntries)
+// (пользователь вводит в консоли числа через пробел и они формируют строку)
+
+
+Console.WriteLine($"Введите числа, через пробел: ");
+string stringArray = (Console.ReadLine()!);
+
+string[] numS = stringArray.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+numS[] = Convert.ToInt32();
+
+
+
+
+
+
+
+
+
+
+
+
+// Задача 43: Написать программу, которая на вход принимает массив из любого количества элементов 
+// (не менее 6)в промежутке от 0 до 100, 
+// а на выходе выводит этот же массив, но отсортированный по возрастанию(от меньшего числа к большему).
+
